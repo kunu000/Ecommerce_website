6533483127819eebc75de5c04823b2992c17678a
kunal Kirola <123616347+kunu000@users.noreply.github.com>
2023-09-01 22:29:30 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,7 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 // add your firebase config here.
-const firebaseConfig = {
-  apiKey: "AIzaSyDnpfrLICUEDlEQIjr0cfnIs3nJkRv3Zak",
-  authDomain: "online-shopping-44f6d.firebaseapp.com",
-  projectId: "online-shopping-44f6d",
-  storageBucket: "online-shopping-44f6d.appspot.com",
-  messagingSenderId: "651749024645",
-  appId: "1:651749024645:web:5a47ee9cd29032051f1aec",
-  measurementId: "G-MZL82FWS8E",
-};
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
